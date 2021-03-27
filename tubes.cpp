b50224a4a439dd4c50f800089dce98fe468b1272
malikrafsan <pro.malikakbar2357@gmail.com>
2021-03-27 18:34:33 +0700

    second commit

---
+++ tubes.cpp
@@ -0,0 +1,13 @@
+/*
+IDENTITAS:
+Nama Anggota 	: 	1. Hafidz Nur Rahman Ghozali		(16520186)
+					2. Malik Akbar Hashemi Rafsanjani	(16520299)
+Topik			: Tugas Besar URO divisi programming
+Deskripsi Tugas :
+	Program robot yang dapat digerakkan ke kanan, kiri, maju, dan mundur, 
+	juga mampu mengkalkulasi jarak antara robot dan musuh kecoak
+	serta mampu menembak kecoak mutan tersebut
+*/
+
+
+
