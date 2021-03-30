71c46a858b2e28fb051e36a7e30a3efe9a1f9c59
malikrafsan <pro.malikakbar2357@gmail.com>
2021-03-30 19:55:22 +0700

    Rombak banyak banget fungsi fungsinya

--- tubes.cpp
+++ tubes.cpp
@@ -19,7 +19,9 @@ typedef struct {
 	int y;
 } Titik ;
 
-char masukan(Titik posisi) {
+bool validBergerak(Titik posisiRobot, Titik posisiKecoa, char pilihan);
+
+char masukan(Titik posisi, Titik posisiKecoa) {
 	/* Menerima input user dan validasi */
 
 	char pilihan;
@@ -33,18 +35,32 @@ char masukan(Titik posisi) {
 		cout << "Ketik 'a' untuk attack, 'f' untuk forward, 'b' untuk backward, 'l' untuk left, 'r' untuk right, 's' untuk stop"<< endl;
 		cin >> pilihan;
 
-		if (posisi.x <= 0 && pilihan == 'l') {
-			cout << "Masukan salah"<<endl;
-			lanjut = false;
-		} else if (posisi.y <= 0 && pilihan == 'b') {
-			cout << "Masukan salah"<<endl;
-			lanjut = false;
-		} else if (pilihan != 'a' && pilihan != 'f' && pilihan != 'b' && pilihan != 'l' && pilihan != 'r' && pilihan != 's') {
-			cout << "Masukan salah"<<endl;
+		if (pilihan == 'a'){
+			lanjut = true;
+		} else if (validBergerak(posisi, posisiKecoa, pilihan)) {
+			lanjut = true;
+		} else {
+			cout << "Masukan invalid, robot tidak dapat melakukan perintah yang diberikan" << endl;
 			lanjut = false;
 		}
 	}
 	return pilihan;
+}
+
+bool validBergerak(Titik posisiRobot, Titik posisiKecoa, char pilihan){
+	if (pilihan == 'f' && (posisiRobot.y + 1 != posisiKecoa.y)){
+		return true;
+	} else if (pilihan == 'r' && (posisiRobot.x + 1 != posisiKecoa.x)){
+		return true;
+	} else if (pilihan == 'b' && posisiRobot.y > 0 && (posisiRobot.y - 1) != posisiKecoa.y) {
+		return true;
+	} else if (pilihan == 'l' && posisiRobot.x > 0 && (posisiRobot.x - 1) != posisiKecoa.x) {
+		return true;
+	} else if (pilihan == 's') {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 Titik bergerak(Titik posisi, char pilihan) {
@@ -52,22 +68,30 @@ Titik bergerak(Titik posisi, char pilihan) {
 
 	if (pilihan == 'f') {
 		posisi.y += 1;
-	} else if (pilihan == 'b' && posisi.y > 0) {
+	} else if (pilihan == 'b') {
 		posisi.y -= 1;
-	} else if (pilihan == 'l' && posisi.x > 0) {
+	} else if (pilihan == 'l') {
 		posisi.x -= 1;
 	} else if (pilihan == 'r') {
 		posisi.x += 1;
+	} else if (pilihan == 's') {
+		posisi.x = -999;
 	}
+
 	return posisi;
 }
 
-Titik dropKecoa(){
-	Titik posisiawal;
-	posisiawal.x = rand() % 40;
-	posisiawal.y = rand() % 40;
-
-	return posisiawal;
+Titik dropKecoa(Titik posisiRobot){
+	Titik posisiKecoa;
+	while (true) {
+		posisiKecoa.x = rand() % 40;
+		posisiKecoa.y = rand() % 40;
+	
+		if (posisiKecoa.x != posisiRobot.x && posisiKecoa.y != posisiRobot.y){
+			break;
+		}
+	}
+	return posisiKecoa;
 }
 
 int nyawa(int health, bool terserang) {
@@ -75,7 +99,7 @@ int nyawa(int health, bool terserang) {
 
 	int pengurangNyawa;
 	
-	/* pengurangNyawa = ... */
+	pengurangNyawa = 1; // Bisa diubah
 
 	if (terserang) {
 		health -= pengurangNyawa;
@@ -83,75 +107,113 @@ int nyawa(int health, bool terserang) {
 	return health;
 }
 
-int hitungJarak(Titik robot, Titik kecoa){
+float hitungJarak(Titik robot, Titik kecoa){
 	float jarak, jarakx, jaraky;
-	jarakx = robot.x-kecoa.y;
-	jaraky = robot.y-kecoa.y;
-	jarak = sqrt(jarakx*jarakx - jaraky*jaraky);
+	jarakx = robot.x - kecoa.y;
+	jaraky = robot.y - kecoa.y;
+	jarak = sqrt(jarakx*jarakx + jaraky*jaraky);
 
 	return jarak;
 }
 
-void serang( Titik posisi, Titik posisiKecoa, float jarakMax){
+bool bisaSerang( Titik posisi, Titik posisiKecoa, float jarakMax){
 	if (hitungJarak(posisi, posisiKecoa) < jarakMax){
-		// lakukan penyerangan
+		return true;
 	} else{
-		cout << "Jarak terlalu jauh, robot tidak bisa menembak" << endl;
+		cout << "Jarak terlalu jauh, robot tidak bisa menembak dan harus bergerak" << endl;
+		return false;
 	}
 }
 
-void info(Titik posisi, int health){
+void info(Titik posisiRobot, Titik posisiKecoa, int health){
 	/* Menampilkan info ke layar */
 	/* Posisi kecoak belum ditampilkan */
 
-	cout << "Posisi robot : ("<<posisi.x<<","<<posisi.y<<")"<<endl;
+	cout << "Posisi robot : ("<<posisiRobot.x<<","<<posisiRobot.y<<")"<<endl;
 	cout << "Nyawa robot : "<<health<<endl;
+	cout << "Posisi kecoa : ("<<posisiKecoa.x<<","<<posisiKecoa.y<<")"<<endl;
 }
 
-void intro(Titik posisikecoa){
+void intro(Titik posisiRobot, Titik posisiKecoa, int health){
 	cout << "Selamat datang dalam program Urang Robot Orang"<<endl;
 	cout <<endl;
 	/* Intronya diubah wae mangga wkwk aku bingung */
-	cout <<  "Posisi awal kecoa : (" << posisikecoa.x << "," << posisikecoa.y << ")" << endl;
+	cout << "Posisi awal robot : (" << posisiRobot.x << "," << posisiRobot.y << ")" << endl;
+	cout << "Nyawa awal robot : " << health <<endl;
+	cout << "Posisi awal kecoa : (" << posisiKecoa.x << "," << posisiKecoa.y << ")" << endl;
 }
 
-void outro(){
+void outro(int countKecoa){
+	cout<<"Robot telah membunuh kecoa sebanyak : " << countKecoa<<endl;
 	cout<< "Program selesai. Terima kasih"<<endl;
 	/* Outro ne diubah yo wkwkw aku bingung*/
 }
 
+Titik totalBergerak(Titik posisiRobot, Titik posisiKecoa, char inp){
+	while (!validBergerak(posisiRobot, posisiKecoa, inp)){
+		cout << "Masukan invalid, robot tidak dapat bergerak ke arah yang dipilih" << endl;
+		cout << "Ketik 'f' untuk forward, 'b' untuk backward, 'l' untuk left, 'r' untuk right, 's' untuk stop"<< endl;
+		cin >> inp;
+	}
+	return bergerak(posisiRobot, inp);
+}
+
+bool diserang(Titik posisiRobot, Titik posisiKecoa) {
+	if (hitungJarak(posisiRobot, posisiKecoa) < 5) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
 int main() {
 	char inp;	
-	Titik posisi, posisiKecoa;
-	int health;
+	Titik posisiRobot, posisiKecoa;
+	int health, countKecoa;
 	bool terserang;
 	float jarakMax = 10;
 
-	posisi.x = 0;
-	posisi.y = 0;
-	health = 20; /* Nyawa awal bisa diubah*/
+	posisiRobot.x = 0;
+	posisiRobot.y = 0;
+	health = 10; /* Nyawa awal bisa diubah*/
+	countKecoa = 0;
 
-	posisiKecoa = dropKecoa();
+	posisiKecoa = dropKecoa(posisiRobot);
 
-	intro(posisi);
+	intro(posisiRobot, posisiKecoa, health);
 
 	while (true) {
-		inp = masukan(posisi);
+		inp = masukan(posisiRobot, posisiKecoa);
 		if (inp == 's') {
 			break; /* Terminasi Program */
 		} else if (inp == 'a') {
 			/* Menyerang */
-			serang(posisi, posisiKecoa, jarakMax);
+			if (bisaSerang(posisiRobot, posisiKecoa, jarakMax)) {
+				countKecoa += 1;
+				cout << "Kecoa " << countKecoa << " telah berhasil dibunuh"<<endl;
+				posisiKecoa = dropKecoa(posisiRobot);
+			} else {
+				cout << "Ketik 'f' untuk forward, 'b' untuk backward, 'l' untuk left, 'r' untuk right, 's' untuk stop"<< endl;
+				cin >> inp;
+
+				posisiRobot = totalBergerak(posisiRobot, posisiKecoa, inp);
+				if (posisiRobot.x == -999) {
+					break;
+				}
+			}
 		} else {
-			posisi = bergerak(posisi, inp);
+			posisiRobot = totalBergerak(posisiRobot, posisiKecoa, inp);
+			if (posisiRobot.x == -999) {
+				break;
+			}
 		}
 
-		/* terserang = diserang(...) */
+		terserang = diserang(posisiRobot, posisiKecoa);
 		health = nyawa(health, terserang);
 
-		info(posisi, health);
+		info(posisiRobot, posisiKecoa, health);
 	}
-	outro();
+	outro(countKecoa);
 
 	return 0;
 }
